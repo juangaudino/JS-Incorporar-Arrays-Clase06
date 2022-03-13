6b590d95f1cb33d6cb8eef709729912f51eb4a50
Juan Gaudino <juangaudino@gmail.com>
2022-03-13 12:00:58 -0300

    Incorporar Arrays

---
+++ README.md
@@ -0,0 +1,5 @@
+# JS Simulador Interactivo Clase04
+
+Simulador Interactivo | Coderhouse | JavaScript
+
+Realización de un sitio web que permita a los usuarios calcular el costo de impresión de diversos materiales.
---
+++ js/index.js
@@ -0,0 +1,72 @@
+/**
+ * @author Juan Gaudino
+ * @version 1.0.0
+ * @description 1er. Entrega Proyecto Final JavaScript - Clase 03 - Coderhouse
+ * 
+ * History
+ *  1.0.0 - 2022-02-23 : Juan Gaudino : Mostrar en consola si el alumno esta aprobado o no
+ */
+
+// Declaración de valores unitarios
+
+const unitarioPAI = 1200;
+const unitarioCPM = 200;
+const metroCuadradoFront = 450;
+const metroCuadradoBack = 730;
+
+
+
+
+// Declaración de funciones
+
+function calcularPrecio(material, cantidad, metrosCuadrados) {
+    let precio = 0;
+    switch (material) {
+        case 'PAI':
+            precio = cantidad * unitarioPAI;
+            break;
+        case 'CPM':
+            precio = cantidad * unitarioCPM;
+            break;
+        case 'Front':
+            precio = metrosCuadrados * metroCuadradoFront;
+            break;
+        case 'Back':
+            precio = metrosCuadrados * metroCuadradoFront;
+            break;
+    }
+    return precio;
+}
+
+function solicitarMaterial() {
+    let material = prompt('Ingrese el material: PAI, CPM, Front o Back');
+    if (material === 'PAI' || material === 'CPM') {
+        cantidad = prompt(`Ingrese la cantidad de ${material} a producir`);
+        console.log(`El costo de producción de ${cantidad} ${material} es de $${calcularPrecio(material, cantidad)}`);
+        alert(`El costo de producción de ${cantidad} ${material} es de $${calcularPrecio(material, cantidad)}`);
+        document.write("<h2> Cotización de "+ cantidad +" "+ material +"</h2>");
+        document.write("<h4> El costo de producción es de $" + calcularPrecio(material, cantidad,) + "</h4>");
+        // document.write(`El costo de producción de ${cantidad} ${material} es de $${calcularPrecio(material, cantidad)}`);
+    }
+    else if (material === 'Front' || material === 'Back') {
+        ancho = prompt(`Ingrese el ancho del material ${material}`);	
+        alto = prompt(`Ingrese el alto del material ${material}`);
+        cantidad = prompt(`Ingrese la cantidad de ${material}`);
+        let metrosCuadrados = (ancho * alto * cantidad);
+        console.log(`Los metros cuadrados del material ${material} son de ${metrosCuadrados}`);
+        console.log(`El costo de producción de ${cantidad} ${material} es de $${calcularPrecio(material, cantidad, metrosCuadrados)}`);
+        alert(`El costo de producción de ${cantidad} ${material} es de $${calcularPrecio(material, cantidad, metrosCuadrados)}`);
+        document.write("<h2> Cotización de "+ cantidad +" "+ material +" "+ "de " + ancho + "x" + alto + " metros" +"</h2>");
+        document.write("<h4> El costo de producción es de $" + calcularPrecio(material, cantidad, metrosCuadrados) + "</h4>");
+        // document.write(`El costo de producción de ${cantidad} ${material} es de $${calcularPrecio(material, cantidad, metrosCuadrados)}`);
+        
+    }
+    return material;
+}
+
+// Solicitud de datos
+
+let material = solicitarMaterial();
+
+
+
