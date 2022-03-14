bb0b4f719124116a33b698bfeebea70442c8f139
Juan Gaudino <juangaudino@gmail.com>
2022-03-14 00:06:48 -0300

    Ajustes Array

--- js/index.js
+++ js/index.js
@@ -13,13 +13,15 @@ const unitarioPAI = 1200;
 const unitarioCPM = 200;
 const metroCuadradoFront = 450;
 const metroCuadradoBack = 730;
-
-
+const metrosCuadrados = (ancho, alto) => {
+    return ancho * alto * cantidad;
+}
 
 
 // Declaración de funciones
 
-function calcularPrecio(material, cantidad, metrosCuadrados) {
+
+calcularPrecio = (material, cantidad, metrosCuadrados) => {
     let precio = 0;
     switch (material) {
         case 'PAI':
@@ -32,11 +34,21 @@ function calcularPrecio(material, cantidad, metrosCuadrados) {
             precio = metrosCuadrados * metroCuadradoFront;
             break;
         case 'Back':
-            precio = metrosCuadrados * metroCuadradoFront;
+            precio = metrosCuadrados * metroCuadradoBack;
             break;
     }
     return precio;
 }
+
+
+// Solicitud de datos
+
+let material = solicitarMaterial();
+let material2 = confirm("Desea agregar otro material?");
+if (material2 == true) {
+    material = solicitarMaterial();
+}
+
 
 function solicitarMaterial() {
     let material = prompt('Ingrese el material: PAI, CPM, Front o Back');
@@ -46,6 +58,7 @@ function solicitarMaterial() {
         alert(`El costo de producción de ${cantidad} ${material} es de $${calcularPrecio(material, cantidad)}`);
         document.write("<h2> Cotización de "+ cantidad +" "+ material +"</h2>");
         document.write("<h4> El costo de producción es de $" + calcularPrecio(material, cantidad,) + "</h4>");
+        precio = calcularPrecio(material, cantidad);
     }
     else if (material === 'Front' || material === 'Back') {
         ancho = prompt(`Ingrese el ancho del material ${material}`);	
@@ -57,13 +70,65 @@ function solicitarMaterial() {
         alert(`El costo de producción de ${cantidad} ${material} es de $${calcularPrecio(material, cantidad, metrosCuadrados)}`);
         document.write("<h2> Cotización de "+ cantidad +" "+ material +" "+ "de " + ancho + "x" + alto + " metros" +"</h2>");
         document.write("<h4> El costo de producción es de $" + calcularPrecio(material, cantidad, metrosCuadrados) + "</h4>");
+        precio = calcularPrecio(material, cantidad, metrosCuadrados);
     }
     return material;
 }
 
-// Solicitud de datos
 
-let material = solicitarMaterial();
+// Creación de objetos
+
+class Producto {
+    constructor(titulo, precio, cantidad) {
+        this.titulo = titulo;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        // this.agregarAlCarrito = function () {
+        //     carrito.push(this);
+        // console.log(`${this.titulo} agregado al carrito`);
+        };
+// }
+}
+
+const PRODUCTO = new Producto(material, calcularPrecio, cantidad);
+
+// Array de Productos
+
+const agregarAlCarrito = (producto) => {
+    carrito.push(producto);
+};
+
+const carrito = [];
+const arrayProductos = [
+    { id: 1, titulo: material, precio: precio, cantidad},
+    { id: 2, titulo: material, precio: precio, cantidad},
+];
+
+agregarAlCarrito(arrayProductos[0, 1]);
+console.log(carrito);
+
+arrayProductos.forEach(productoEnCarrito => {
+    console.log(productoEnCarrito)
+});
+    
+// agregarAlCarrito(arrayProductos[0]);
+// console.log(carrito);
+
+
+// console.log("Esto devuelve el length del array: " + arrayProductos.length);
+// for (let i = 0; i < arrayProductos.length; i++) {
+//     console.log(arrayProductos[i]);
+// }
+
+// Carrito
+
+// carrito.push(agregarAlCarrito);
+// alert(`${PRODUCTO.titulo} agregado al carrito`);
+// console.log(carrito);
+
+// const indiceDelProducto = carrito.indexOf(PRODUCTO);
+// console.log(indiceDelProducto);
+
 
 
 
